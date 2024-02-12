a2654b855771d786eb7782725f86c6cb0de7b6b0
Muhammad Tayyab Sheikh <me@cstayyab.com>
2024-02-12 16:38:39 +0500

    ✨ feat: Enhance deep linking support for iOS and Android
    This commit introduces significant enhancements to the deep linking capabilities of the Express.js application, adding support for Android devices alongside the existing iOS deep linking functionality. By leveraging environment variables for Android bundle ID and a universal deep link scheme, the application now dynamically redirects users to the appropriate app store or attempts to open the app directly if it's installed on the user's device.
    
    Key Additions:
    - Introduced `androidBundleId` and `deeplinkScheme` variables, sourced from environment variables, to support Android deep linking.
    - Implemented a new route `/${iOSBundleId}/*` that serves an HTML page executing JavaScript to handle deep linking. This script checks the user's device type (iOS or Android) and attempts to redirect to the app using the specified deep link scheme. If the app is not installed, it redirects the user to the respective app store (Apple App Store for iOS devices and Google Play Store for Android devices) after a brief delay, allowing for the app to open if it is installed.
    
    Enhancements:
    - The dynamic redirection script enhances the user experience by providing a seamless transition from web to app, improving engagement and streamlining the process of acquiring or returning to the app.
    - By using environment variables for configuration, the implementation remains flexible and easily adaptable to different deployment environments or app configurations.
    
    This update significantly broadens the application's utility by accommodating users on both major mobile platforms, facilitating a more inclusive and user-friendly approach to deep linking and app promotion.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const pkgInfo = require('./package.json')
 
 const iOSBundleId = process.env.IOS_BUNDLE_ID || process.env.BUNDLE_ID
 const iOSTeamId = process.env.TEAMID
+const androidBundleId = process.env.ANDROID_BUNDLE_ID || process.env.BUNDLE_ID
+const deeplinkScheme = process.env.DEEP_LINK_SCHEME
 const univeralLink = {
     "applinks": {
         "apps": [],
@@ -32,6 +34,50 @@ app.get('/.well-known/apple-app-site-association', (req, res) => {
         })
     }
 })
+
+app.get('/${iOSBundleId}/*', (res, req) => {
+    res.status(200).send(`
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Redirect to App or Store</title>
+</head>
+<body>
+    <script>
+        function redirectToAppOrStore() {
+            var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+            var deepLink = '${deeplinkScheme}://';
+            var storeLink;
+
+            if (isMobile) {
+                // Define deep link and store links
+                if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+                    storeLink = 'https://apps.apple.com/app/${iOSBundleId}';
+                } else if (/Android/i.test(navigator.userAgent)) {
+                    storeLink = 'https://play.google.com/store/apps/details?id=${androidBundleId}';
+                }
+
+                // Attempt to redirect to the deep link
+                window.location.href = deepLink;
+
+                // Redirect to the store if the app is not installed
+                setTimeout(function() {
+                    window.location.href = storeLink;
+                }, 2500); // Adjust timeout as needed
+            } else {
+                // Optionally handle desktop users or show a message
+                alert('Please visit this page on a mobile device.');
+            }
+        }
+
+        // Run the redirect function when the page loads
+        window.onload = redirectToAppOrStore;
+    </script>
+</body>
+</html>
+
+    `)
+});
 
 app.listen(port, () => {
   console.log(`Deferred Deeplinkg Demo App listening on port ${port}`)
