8b09913f8c3e3aff943d8b7b989d1864c01f532f
Muhammad Tayyab Sheikh <me@cstayyab.com>
2024-02-12 15:02:47 +0500

    🚀 feat: Initialize Express app with deferred deep linking support
    This commit introduces the foundational setup for a new Express application tailored for testing deferred deep linking capabilities. The changes include the creation of a basic server setup in `index.js`, a comprehensive `.gitignore` file to exclude unnecessary files from version control, and the initial `package.json` file defining project metadata and dependencies.
    
    Key components added:
    - `index.js`: Sets up an Express server with two routes. The root route (`/`) serves a welcome message and author information, while the `/.well-known/apple-app-site-association` route provides the necessary JSON for iOS Universal Links, supporting deferred deep linking by dynamically generating links based on environment variables for the iOS bundle ID and team ID.
    - `.gitignore`: Ensures that node modules, logs, environment files, and other non-essential files are excluded from the repository, keeping the project clean and focused.
    - `package.json`: Defines the project's name, version, description, main entry point, scripts, keywords, author information, license, and dependencies, including Express and dotenv for environment variable management.
    
    This setup lays the groundwork for further development and testing of deferred deep linking functionality, ensuring a clean, maintainable, and efficient workflow.
    
    Dependencies added:
    - express: ^4.18.2 for server functionality
    - dotenv: ^16.4.2 for environment variable management
    
    The commit reflects a significant step towards building a demo for deferred deep linking, focusing on clean codebase management and essential functionalities.

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const pkgInfo = require('./package.json')
+
+const iOSBundleId = process.env.IOS_BUNDLE_ID || process.env.BUNDLE_ID
+const iOSTeamId = process.env.TEAMID
+const univeralLink = {
+    "applinks": {
+        "apps": [],
+        "details": [{
+            "appID": `${iOSTeamId}.${iOSBundleId}`,
+            "paths": [`/${iOSBundleId}/*`]
+            }
+        ]
+    }
+}
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to Deferred Deep Link Testing Demo!',
+    author: pkgInfo.author
+  })
+})
+
+app.get('/.well-known/apple-app-site-association', (req, res) => {
+    if(iOSBundleId && iOSTeamId) {
+        res.json(univeralLink)
+    } else {
+        res.status(404).json({
+            error: 'Team ID or Bundle ID is missing.'
+        })
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Deferred Deeplinkg Demo App listening on port ${port}`)
+})
