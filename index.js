61861fde46db51f0a8a054956a8e793a5ebe9404
Muhammad Tayyab Sheikh <me@cstayyab.com>
2024-02-12 16:55:05 +0500

    🚀 feat: Refactor deep link redirect and fix request handler parameter order
    This commit introduces a significant refactor to the deep linking functionality of the Express.js application, alongside a critical bug fix for the parameter order in the deep link route handler.
    
    Changes made:
    - Corrected the order of parameters in the deep link route handler from `(res, req)` to `(req, res)`, fixing a bug where the request object was incorrectly referenced, leading to a `TypeError` when attempting to read `req.params[0]`.
    - Extracted the HTML generation for the deep link redirection page into a separate function `redirectPage(deepLinkPath)`, enhancing code readability and maintainability. This function dynamically generates the redirect script based on the provided `deepLinkPath`, facilitating more flexible and reusable redirection logic.
    - Updated the deep link route handler to use the new `redirectPage` function, streamlining the response generation process and ensuring that the correct deep link path is used in the redirection script.
    
    This refactor not only corrects a critical bug that affected the application's ability to handle dynamic deep link paths but also significantly improves the structure of the code related to deep linking. By isolating the redirection logic into a dedicated function, the application becomes easier to maintain and extend, with clearer separation of concerns and enhanced readability.

--- index.js
+++ index.js
@@ -18,6 +18,48 @@ const univeralLink = {
     }
 }
 
+const redirectPage = (deepLinkPath) => `
+<!DOCTYPE html>
+<html>
+<head>
+<title>Redirect to App or Store</title>
+</head>
+<body>
+<script>
+    function redirectToAppOrStore() {
+        var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        var deepLink = '${deeplinkScheme}://${deepLinkPath}';
+        var storeLink;
+
+        if (isMobile) {
+            // Define deep link and store links
+            if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+                storeLink = 'https://apps.apple.com/app/${iOSBundleId}';
+            } else if (/Android/i.test(navigator.userAgent)) {
+                storeLink = 'https://play.google.com/store/apps/details?id=${androidBundleId}';
+            }
+
+            // Attempt to redirect to the deep link
+            window.location.href = deepLink;
+
+            // Redirect to the store if the app is not installed
+            setTimeout(function() {
+                window.location.href = storeLink;
+            }, 2500); // Adjust timeout as needed
+        } else {
+            // Optionally handle desktop users or show a message
+            alert('Please visit this page on a mobile device.');
+        }
+    }
+
+    // Run the redirect function when the page loads
+    window.onload = redirectToAppOrStore;
+</script>
+</body>
+</html>
+
+`
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to Deferred Deep Link Testing Demo!',
@@ -35,49 +77,9 @@ app.get('/.well-known/apple-app-site-association', (req, res) => {
     }
 })
 
-app.get(`/${iOSBundleId}/*`, (res, req) => {
+app.get(`/${iOSBundleId}/*`, (req, res) => {
     const deepLinkPath = req.params[0];
-    res.status(200).send(`
-    <!DOCTYPE html>
-<html>
-<head>
-    <title>Redirect to App or Store</title>
-</head>
-<body>
-    <script>
-        function redirectToAppOrStore() {
-            var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-            var deepLink = '${deeplinkScheme}://${deepLinkPath}';
-            var storeLink;
-
-            if (isMobile) {
-                // Define deep link and store links
-                if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-                    storeLink = 'https://apps.apple.com/app/${iOSBundleId}';
-                } else if (/Android/i.test(navigator.userAgent)) {
-                    storeLink = 'https://play.google.com/store/apps/details?id=${androidBundleId}';
-                }
-
-                // Attempt to redirect to the deep link
-                window.location.href = deepLink;
-
-                // Redirect to the store if the app is not installed
-                setTimeout(function() {
-                    window.location.href = storeLink;
-                }, 2500); // Adjust timeout as needed
-            } else {
-                // Optionally handle desktop users or show a message
-                alert('Please visit this page on a mobile device.');
-            }
-        }
-
-        // Run the redirect function when the page loads
-        window.onload = redirectToAppOrStore;
-    </script>
-</body>
-</html>
-
-    `)
+    res.status(200).send(redirectPage(deepLinkPath))
 });
 
 app.listen(port, () => {
